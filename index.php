63bf6246dcd62ef55c24ceee2a65c37649a11092
H_Apple |A.S.A.P| <39252325+HumanAdjust@users.noreply.github.com>
2019-04-18 20:22:33 +0900

    Upload First shift
    doh.

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset=”utf-8”>
+<title>Jtech shopping mall</title>
+<link href="css/main.css" rel="stylesheet" type="text/css">   <!-- Navigation CSS -->
+<link rel="stylesheet" href="navbar_techandall.css">
+<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+ <header>
+   <center>
+   <div id="logo">
+     <img src="imgs/Jtech_logo.png" alt="Jtech" width="176" height="67">
+   </div>
+   <div id="head_link">
+     <ul>
+       <li><a href="#">로그인</a></li>
+       <li><a href="#">회원가입</a></li>
+       <li><a href="#">장바구니</a></li>
+       <li><a href="#">고객센터</a></li>
+       <li><a href="#">주문배송</a></li>
+     </ul>
+   </div>
+   <br />
+   <div class="container">
+   <div>
+      <label class="mobile_menu" for="mobile_menu">
+        <span>Menu</span>
+      </label>
+      <input id="mobile_menu" type="checkbox">
+      <ul class="nav">
+        <li><a href="#"><i class="icon-home icon-large"></i></a></li>
+        <li class="dropdown"><a href="#">blueb.co.kr</a>
+      <div class="fulldrop">
+      <div class="column">
+          <h3>menu1</h3>
+          <ul>
+            <li><a href="#">Tech ANd All</a></li>
+            <li><a href="#">Web Designs</a></li>
+            <li><a href="#">PSD</a></li>
+            <li><a href="#">Scripts</a></li>
+          </ul>
+      </div>
+    <div class="column">
+        <h3>Menu Header</h3>
+        <ul>
+          <li><a href="#">Tech ANd All</a></li>
+          <li><a href="#">Web Designs</a></li>
+          <li><a href="#">PSD</a></li>
+          <li><a href="#">Scripts</a></li>
+        </ul>
+    </div>
+    <div class="column">
+        <h3>Menu Header</h3>
+        <ul>
+          <li><a href="#">Tech ANd All</a></li>
+          <li><a href="#">Web Designs</a></li>
+          <li><a href="#">PSD</a></li>
+          <li><a href="#">Scripts</a></li>
+        </ul>
+    </div>
+
+    <div class="column">
+        <h3>Menu Header</h3>
+        <ul>
+          <li><a href="#">Tech ANd All</a></li>
+          <li><a href="#">Web Designs</a></li>
+          <li><a href="#">PSD</a></li>
+          <li><a href="#">Scripts</a></li>
+        </ul>
+    </div>
+    <div class="column">
+        <h3>Menu Header</h3>
+        <ul>
+          <li><a href="#">Tech ANd All</a></li>
+          <li><a href="#">Web Designs</a></li>
+          <li><a href="#">PSD</a></li>
+          <li><a href="#">Scripts</a></li>
+        </ul>
+    </div>
+
+
+    <div class="column">
+        <h3>Menu Header</h3>
+        <ul>
+          <li><a href="#">Tech ANd All</a></li>
+          <li><a href="#">Web Designs</a></li>
+          <li><a href="#">PSD</a></li>
+          <li><a href="#">Scripts</a></li>
+        </ul>
+      </div>
+    </div>
+    </li>
+    <li class="dropdown"><a href="#">Drop Down Menu 2</a>
+    <div class="fulldrop">
+    <div class="column">
+   		menu2
+   	</div>
+      </li>
+      <li><a href="#">  Web Designs</a></li>
+      <li><a href="#"> LifeStyle</a></li>
+      <li><a href="#"> About</a></li>
+      <li><a href="#"> Contact</a></li>
+      <li class="search"><form action="Search.php">
+      <i class="icon-search icon-large"></i><input type="text">
+      </form>
+      </li>
+      </ul>
+    </div>
+  </div>
+  </center>
+  </header>
+ <nav> </nav>
+ <article> </article>
+ <footer> </footer>
+</body>
+<html>
